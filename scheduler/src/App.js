d490e9acf08d94836c7e3692a231f9d235ded16c
taehunkim115 <35262234+taehunkim115@users.noreply.github.com>
2019-09-27 16:18:33 -0500

    slice 4: fetch data

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import 'rbx/index.css';
 import { Button, Container, Title } from 'rbx';
 
@@ -29,7 +29,7 @@ const schedule = {
 };
 
 const Banner = ({ title }) => (
-  <Title>{ title }</Title>
+  <Title>{ title || '[loading...]' }</Title>
 );
 
 const CourseList = ({ courses }) => (
@@ -54,11 +54,26 @@ const Course = ({ course }) => (
   </Button>
 );
 
-const App = () =>  (
-  <Container>
-    <Banner title={ schedule.title } />
-    <CourseList courses={ schedule.courses } />
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState({ title: '', courses: [] });
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+    fetchSchedule();
+  }, [])
+
+  return (
+    <Container>
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </Container>
+  );
+};
 
 export default App;
